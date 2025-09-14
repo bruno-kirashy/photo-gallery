6d847c2fb56f50c227b9b881a1d1017e9771fcb6
Bruno Santos Silva <jkbrunowk@hotmail.com>
2025-09-14 14:52:14 -0300

    Concertando responsividade para mobile.

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -11,14 +11,14 @@ export const Gallery = ({gallery, handleClickPhoto, darkMode}:GalleryProp) => {
     return(
         <ul className="flex items-center justify-center flex-wrap ">
             {gallery.map((item, index) => (
-            <li className="flex items-center justify-center flex-col cursor-pointer" key={index}>
+            <li className="flex items-center justify-center flex-col cursor-pointer mx-2" key={index}>
                 <img 
                 onClick={() => handleClickPhoto(item.img)}
                 src={`/Images/${item.img}.jpg`} 
                 alt={item.description} 
-                className={`w-70 h-40 m-3 md:w-90 md:h-50 border-1 transition-all duration-500 rounded-md hover:scale-103 ${darkMode ? 'border-gray-600/20' : 'border-gray-900/50 shadow-md shadow-black/20'}`}
+                className={`w-auto h-auto my-3 md:w-90 md:h-50 border-1 transition-all duration-500 rounded-md hover:opacity-100 hover:shadow-2xl shadow-blue-500/10 hover:scale-103 ${darkMode ? 'border-gray-600/20 opacity-90' : 'border-gray-900/50 shadow-md shadow-black/20'}`}
                 />
-                <h1 className={`w-80 md:h-20 h-8 font-mono md:text-[14px] text-[10px] text-gray-400 text-center transition-all duration-300 ${darkMode ? 'text-gray-400' : 'text-gray-800'}`}>{item.description}</h1>
+                <h1 className={`w-auto mx-2 md:h-20 h-8 font-mono md:text-[14px] text-[13px] text-gray-400 text-center transition-all duration-300 ${darkMode ? 'text-gray-400' : 'text-gray-800'}`}>{item.description}</h1>
             </li>
             ))}
         </ul>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,17 +37,16 @@ const App = () => {
   return (
     
     <div className={`w-full flex items-center justify-center flex-col transition-all duration-300 ${darkMode ? 'bg-black' : 'bg-white'}`}>
-      <div className="w-full h-full border-b-1 flex justify-end items-center border-gray-600 md:px-20 px-7">
-        <h1 className={`font-mono text-[8px] mr-2 md:text-[13px] transition-all duration-300 ${darkMode ? 'text-white' : 'text-black'}`}>Modo Escuro</h1>
+      <div className="w-full h-full border-b-1 flex justify-end items-center border-gray-600 md:px-20 px-2">
         {darkMode ? 
-          <Sun onClick={selectDarkMode}  className="mr-2 md:w-9 md:h-9 md:border-2  w-7 h-7 cursor-pointer my-2  border-1 rounded-4xl p-1 transition-all duration-300 hover:text-blue-400 hover:shadow-md hover:shadow-blue-500/50"/>
-           : <Moon  onClick={selectDarkMode} className={`mr-2 md:w-9 md:h-9 md:border-2 w-7 h-7 cursor-pointer my-2 shadow-2xl shadow-black  border-1 rounded-4xl p-1 transition-all duration-300 hover:text-blue-400 hover:shadow-md hover:shadow-blue-500/50 ${darkMode ? 'text-white border-black' : 'bg-black'}`} /> }
+          <Sun onClick={selectDarkMode}  className="mr-1 md:w-9 md:h-9 md:border-2  w-9 h-9 cursor-pointer my-2  border-1 rounded-4xl p-1 transition-all duration-300 hover:text-blue-400 hover:shadow-md hover:shadow-blue-500/50"/>
+           : <Moon  onClick={selectDarkMode} className={`mr-1 md:w-9 md:h-9 md:border-2 w-9 h-9 cursor-pointer my-2 shadow-2xl shadow-black  border-1 rounded-4xl p-1 transition-all duration-300 hover:text-blue-400 hover:shadow-md hover:shadow-blue-500/50 ${darkMode ? 'text-white border-black' : 'bg-black'}`} /> }
           
       </div>
-      <h1 className={`md:m-8 m-4 text-2xl font-mono md:text-5xl transition-all duration-300 ${darkMode ? 'text-white' : 'text-black'}`}>
+      <h1 className={`md:m-8 m-4 text-3xl font-mono md:text-5xl transition-all duration-300 ${darkMode ? 'text-white' : 'text-black'}`}>
         Galeria De Fotos
       </h1>
-      <h6 className={`md:mb-10 mb-3 text-[12px] w-[320px]  md:text-[17px] font-mono text-center md:w-auto transition-all duration-300 ${darkMode ? 'text-gray-400' : 'text-gray-800'}`}>
+      <h6 className={`md:mb-10 mb-3 text-[14px] mx-2  md:text-[17px] font-mono text-center md:w-auto transition-all duration-300 ${darkMode ? 'text-gray-400' : 'text-gray-800'}`}>
         Capturando momentos inesquecíveis, uma imagem de cada vez.
       </h6>
       
